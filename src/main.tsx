1f217f70dc1eb64ac5c6fe45d837d57ea2eef607
Sarfaraz Tanwri <hpcorei057@gmail.com>
2026-03-18 07:43:07 +0500

    final featured slider working

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { SpeedInsights } from "@vercel/speed-insights/react";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById("root")!).render(<App />);
 
